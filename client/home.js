c109ed2365ea50e9f8aa99c2d6e80a188cee2025
lovepsm94 <64115961+lovepsm94@users.noreply.github.com>
2020-11-13 22:18:42 +0700

    add loading animation + search question

--- client/home.js
+++ client/home.js
@@ -5,20 +5,24 @@ function renderPage() {
         url: "https://test1234312312.herokuapp.com/random-question",
         type: "GET",
         success: (res) => {
-            $('#content').text(`${res.content}`)
-            questionId = res._id
-            document.querySelector('.result-btn').disabled = false
+            setTimeout(() => {
+                $('#content').html(`${res.content}`)
+                questionId = res._id
+                document.querySelector('.result-btn').disabled = false
+            }, 500)
+
         },
         error: (err) => console.log(err)
     })
 }
 renderPage()
-document.querySelector('.result-btn').addEventListener('click',() => {
+document.querySelector('.result-btn').addEventListener('click', () => {
     location.replace(`https://test1234312312.herokuapp.com/question/${questionId}`)
 })
-document.querySelector('.next-question-btn').addEventListener('click',() => {
-    renderPage()
+document.querySelector('.next-question-btn').addEventListener('click', () => {
+    location.replace('https://test1234312312.herokuapp.com')
 })
+
 function upVote() {
     $.ajax({
         url: "https://test1234312312.herokuapp.com/question",
